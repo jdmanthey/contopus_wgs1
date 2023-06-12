960b3a000717bb1cb0a0564e4d47f80831645036
Joe Manthey <jdmanthey@gmail.com>
2023-06-12 16:35:18 -0500

    Update 13_cat_window_output.sh

--- 13_cat_window_output.sh
+++ 13_cat_window_output.sh
@@ -2,18 +2,14 @@ cd /lustre/scratch/jmanthey/15_contopus/06_window_stats/windows
 
 # combine the output for different analyses into a single file each
 # first add a header for each file
-grep 'pop1' CHR_10__10000001__10100000__stats.txt > ../window_heterozygosity.txt
-grep 'pop1' CHR_10__10000001__10100000__stats.txt > ../window_theta.txt
-grep 'pop1' CHR_10__10000001__10100000__stats.txt > ../window_pi.txt
-grep 'pop1' CHR_10__10000001__10100000__stats.txt > ../window_Tajima_D.txt
-grep 'pop1' CHR_10__10000001__10100000__stats.txt > ../window_Dxy.txt
-grep 'pop1' CHR_10__10000001__10100000__stats.txt > ../window_Fst.txt
+grep 'pop1' CHR_10__10000001__10050000__stats.txt > ../window_heterozygosity.txt
+grep 'pop1' CHR_10__10000001__10050000__stats.txt > ../window_pi.txt
+grep 'pop1' CHR_10__10000001__10050000__stats.txt > ../window_Dxy.txt
+grep 'pop1' CHR_10__10000001__10050000__stats.txt > ../window_Fst.txt
 
 # add the relevant stats to each file
 for i in $( ls *txt ); do grep 'heterozygosity' $i >> ../window_heterozygosity.txt; done
-for i in $( ls *txt ); do grep 'theta' $i >> ../window_theta.txt; done
 for i in $( ls *txt ); do grep 'pi' $i >> ../window_pi.txt; done
-for i in $( ls *txt ); do grep 'Tajima_D' $i >> ../window_Tajima_D.txt; done
 for i in $( ls *txt ); do grep 'Dxy' $i >> ../window_Dxy.txt; done
 for i in $( ls *txt ); do grep 'Fst' $i >> ../window_Fst.txt; done
 
