b13e6c95b88763230be23912cff7c53471ac6ec4
Joe Manthey <jdmanthey@gmail.com>
2023-09-27 10:39:15 -0500

    Update 14a_cat_for_ldhat.sh

--- 14a_cat_for_ldhat.sh
+++ 14a_cat_for_ldhat.sh
@@ -7,3 +7,12 @@ grep "#" CHR_29_eastern.recode.vcf > eastern_ldhat.vcf
 for i in $( ls *eastern.recode.vcf ); do grep -v "#" $i >> eastern_ldhat.vcf; done
 
 for i in $( ls *western.recode.vcf ); do grep -v "#" $i >> western_ldhat.vcf; done
+
+# bgzip and tabix index vcfs
+bgzip eastern_ldhat.vcf
+#tabix
+tabix -p vcf eastern_ldhat.vcf.gz
+
+bgzip western_ldhat.vcf
+#tabix
+tabix -p vcf western_ldhat.vcf.gz
