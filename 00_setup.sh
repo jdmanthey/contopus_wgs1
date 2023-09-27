c9bad1d3d004d5d77d0bc12ddd2cd1419f089d4d
Joe Manthey <jdmanthey@gmail.com>
2023-09-27 10:02:00 -0500

    Update 00_setup.sh

--- 00_setup.sh
+++ 00_setup.sh
@@ -12,8 +12,10 @@ mkdir 03_vcf
 mkdir 04_filtered_vcf
 mkdir 05_structure
 mkdir 06_window_stats
+mkdir 06_window_stats/windows
 mkdir 07_find_fixed_differences
-mkdir 08_relernn
+mkdir 08_ldhat
+mkdir 08_ldhat/windows
 mkdir 09_demography
 mkdir 10_filter
 mkdir 12_filter_vcf
